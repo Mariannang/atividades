0a49316e5b57a9a9529b6d68fe94d802ccd4b2e6
Mariannang <mariannagoncalves911@gmail.com>
2025-10-02 21:48:43 -0300

    Update Atividade_avaliativa_01_c.py

--- Atividade_avaliativa_01_c.py
+++ Atividade_avaliativa_01_c.py
@@ -1,31 +1,73 @@
 #criar calculadora com as 4 operacoes
+start_programa = True
+recebe_campos = True
+campo = 1
 print("-----CALCULADORA-----")
-#entrada de dados
-n1 = float(input("Digite o primeiro numero"))
-n2 = float(input("Digite o segundo numero"))
+while start_programa:
+    while recebe_campos:
+         match campo:
+            case 1:
+                try:
+                    #entrada de dados
+                    n1 = float(input("Digite o primeiro numero"))
+                    campo += 1
+                except:
+                        print("Valor invalido...")
 
-print("\nEscolha a operacao:")
-print("1 - Soma (+)")
-print("2 - Subtracao (-)")
-print("3 - Multiplicacao (*)")
-print("4 - Divisao (/)")
+            case 2:
+                try:
+                    n2 = float(input("Digite o segundo numero"))
+                    campo += 1
+                except:
+                        print("Valor invalido...")
+            case 3:
+                try:
+                    print("\nEscolha a operacao:")
+                    print("1 - Soma (+)")
+                    print("2 - Subtracao (-)")
+                    print("3 - Multiplicacao (*)")
+                    print("4 - Divisao (/)")
 
-operacao = input ("digite a operacao")
+                    operacao = str(input ("digite a operacao"))
+                    recebe_campos = false
+                except:
+                    print("Valor invalido...")
+
+    else:
+
 #processamento
-if operacao =="1":
-resultado + n1 = n2
-print(f"\nresultado: {n1} + {n2} = {resultado}")
-elif operacao == "2":
-resultado = n1 - n2
-print(f"\nResultado: {n1} - {n2} = {resultado}")
-elif operacao == "3":
-resultado = n1 * n2
-print(f"\nResultado: {n1} * {n2} = {resultado}")
-elif operacao == "4"
-if n2 != 0:
-    resultado = n1 / n2
-    print(f"\nResultado: {n1} / {n2} = {resultado}")
-else:
-    print("\nErro: Divisao por zero não é permitido")
+            if operacao =="1":
+              resultado = n1 + n2
+            print(f"\nresultado: {n1} + {n2} = {resultado}")
+            elif operacao == "2":
+            resultado = n1 - n2
+            print(f"\nResultado: {n1} - {n2} = {resultado}")
+            elif operacao == "3":
+            resultado = n1 * n2
+            print(f"\nResultado: {n1} * {n2} = {resultado}")
+            elif operacao == "4"
+            if n2 != 0:
+                resultado = n1 / n2
+                print(f"\nResultado: {n1} / {n2} = {resultado}")
+    
+                print("\nErro: Divisao por zero não é permitido")
 else: print("\nOperacao invalida")
+
+fim_programa = True
+while fim_programa:
+    print("\nDeseja finalizar o programa?\n")
+    try:
+        confirma = str(input("Digite S (SIM) ou N (calcule novamente): "))
+        if confirma == "s" or confirma "S":
+        fim_programa = False
+        start_programa = False
+        if confirma == "n" or confirma "N":
+            fim_programa = False
+            recebe_campos = True
+            campo = 1
+            except:
+                print("Operacao invalida...")
+else:
+    print("\n----------------CONCLUIDO-----------------\n")
+
 
